5504810641104bfef1683bd821aa0c4e08179890
David <david.owuor@student.moringaschool.com>
2023-03-06 23:53:41 +0300

    complete

--- db/migrate/20230306202753_create_reviews.rb
+++ db/migrate/20230306202753_create_reviews.rb
@@ -1,9 +1,12 @@
+
+
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.integer :score
       t.string :comment
       t.integer :game_id # this is our foreign key
+      t.integer :user_id
       t.timestamps
     end
   end
